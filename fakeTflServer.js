1d2968e730d8f6bfe7bf7ba58388d62b044829a9
Josh <joshmvince@gmail.com>
2016-11-17 20:19:06 +0000

    moved tfl server into server for dev

--- fakeTflServer.js
+++ fakeTflServer.js
@@ -1,21 +1,35 @@
-var dotenv = require('dotenv')
-dotenv.load()
-var express = require('express')
-var morgan = require('morgan')
-const debug = require('debug')('server')
-var app = express()
+module.exports = function(){
+  var dotenv = require('dotenv')
+  dotenv.load()
+  var express = require('express')
+  var morgan = require('morgan')
+  const debug = require('debug')('server')
+  var app = express()
 
-app.use(morgan('dev'))
+  app.use(morgan('dev'))
 
-app.get('/Line/:id/Disruption', (req, res, next) => {
-  res.send({"status": "GoodService", "line": `${req.params.id}`})
-})
+  app.get('/Line/northern/Disruption', (req, res, next) => {
+    res.send(sendGoodService())
+  })
 
-app.use((err, req, res, next) => {
-  console.log("Special Error Log: " + err)
-  res.status(500).send("Oops! Something went wrong")
-})
+  app.get('/Line/:id/Disruption', (req, res, next) => {
+    res.send(sendDelay())
+  })
 
-app.listen(5000, () => {
-  console.log("Fake TFL is listening on port 5000")
-})
+  app.use((err, req, res, next) => {
+    console.log("Special Error Log: " + err)
+    res.status(500).send("Oops! Something went wrong")
+  })
+
+  app.listen(5000, () => {
+    console.log("Fake TFL is listening on port 5000")
+  })
+
+  function sendDelay(){
+    return {"$type":"Tfl.Api.Presentation.Entities.Disruption, Tfl.Api.Presentation.Entities","category":"RealTime","type":"routeInfo","categoryDescription":"RealTime","description":"Bakerloo Line: Minor delays between Queens Park and Harrow & Wealdstone due to an earlier signal failure at Queens Park .GOOD SERVICE on the rest of the line. ","affectedRoutes":[],"affectedStops":[],"closureText":"minorDelays"}
+  }
+
+  function sendGoodService(){
+    return []
+  }
+}
--- server.js
+++ server.js
@@ -8,39 +8,42 @@ var app = express()
 // LOG THE REQUESTS IN DEV MODE FOR NOW
 app.use(morgan('dev'))
 
+// START THE FAKETFLSERVER
+var fakeTflServer = require('./fakeTflServer.js')
+fakeTflServer();
+
 var TubeStatus = require('./lib/TubeStatus.js').TubeStatus;
 var SupportedLines = require('./lib/SupportedLines.js')
-
 var lines = SupportedLines()
 
 
 // RENDER A BASIC HOMEPAGE AT ROOT 
 //TODO: extract out the rendering stuff to a separate module!
 app.get('/', (req, res) => {
-	var title = `<h1> Tube Status </h1><br>`
+  var title = `<h1> Tube Status </h1><br>`
 
-	function links(lines){
-		return lines.map((el) =>{
-			return `<a href="/lines/${el.lowercase}">${el.name} Line</a><br>`
-		})
-	}
-	
-	res.send(title + links(lines))
+  function links(lines){
+    return lines.map((el) =>{
+      return `<a href="/lines/${el.lowercase}">${el.name} Line</a><br>`
+    })
+  }
+  
+  res.send(title + links(lines))
 })
 
 app.get('/lines/:id', (req, res, next) => {
-	TubeStatus(req.params.id).then(data => {
-		res.send(data)
-	})
+  TubeStatus(req.params.id).then(data => {
+    res.send(data)
+  })
 })
 
 
 // ERROR HANDLER
 app.use((err, req, res, next) => {
-	console.log("Special Error Log: " + err)
-	res.status(500).send("Oops! Something went wrong")
+  console.log("Special Error Log: " + err)
+  res.status(500).send("Oops! Something went wrong")
 })
 
 app.listen(3000, () => {
-	console.log("Listening on port 3000")
+  console.log("Listening on port 3000")
 })
